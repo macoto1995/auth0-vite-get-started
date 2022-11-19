f0fd5cdc113d9af97ad50ecb6d3f99e966e3fdde
Makoto <adosute07@gmail.com>
2022-11-19 16:27:21 +0900

    Login

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import App from './App'
 import './index.css'
+import { Auth0Provider } from "@auth0/auth0-react";
+import { AUTH0_CALLBACK_URL, AUTH0_CLIENT_ID, AUTH0_DOMAIN } from 'src/lib/env';
+import { Login } from './pages/login';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={AUTH0_DOMAIN}
+      clientId={AUTH0_CLIENT_ID}
+      redirectUri={AUTH0_CALLBACK_URL}
+    >
+      <RouterProvider router={router} />
+    </Auth0Provider>
   </React.StrictMode>
 )
---
+++ src/pages/login.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import LoginButton from 'src/components/login-button'
+
+export const Login = () => {
+  return (
+    <LoginButton />
+  )
+}
